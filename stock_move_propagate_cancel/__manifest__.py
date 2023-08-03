83800eba6bc8377b8684e3223564302c292c505d
Florian da Costa <florian.dacosta@akretion.com>
2023-08-03 21:50:37 +0200

    [ADD] stock_move_propagate_cancel

---
+++ stock_move_propagate_cancel/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Stock Move Propagate Cancel",
+    "summary": "Manage stock move partial cancel propagation",
+    "version": "16.0.1.1.0",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["florian-dacosta"],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock"],
+}
